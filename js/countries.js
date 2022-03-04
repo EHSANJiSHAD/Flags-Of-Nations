5eb1b9de306feb250e1bf59b1cc7faeaff4c0905
EHSANJiSHAD <tanvirehsan60@gmail.com>
2022-03-04 12:37:33 +0600

    search option added

--- js/countries.js
+++ js/countries.js
@@ -1,24 +1,47 @@
 const loadCountries = () => {
+    
+    document.getElementById('loader').style.display = 'block';
+
+
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => displayCountries(data));
 }
 
 loadCountries();
-const displayCountries = countries => {
-    const countryDiv = document.getElementById('countries');
 
-    // for(const country of countries){
 
-    // }
+const searchedCountry = () => {
+    document.getElementById('loader').style.display = 'block';
 
-    countries.forEach(country => {
+    const searchText = document.getElementById('input-field').value;
+
+    fetch(`https://restcountries.com/v3.1/name/${searchText}`)
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+
+// displayCountries(data)
+
+
+const displaySearchedCountry = country => {
+    const countryDiv = document.getElementById('single-country');
+    console.log(country);
+    countryDiv.innerHTML = '';
+    country.forEach(country => {
         const h3 = document.createElement('h3');
         const div = document.createElement('div');
         div.classList.add('country');
         div.innerHTML = `
-            <h3>${country.name.common}</h3>
-            <button  onclick="loadCountryName('${country.name.official}')" id="detail-btn"><a id="link" href="#top">DETAILS</a></button>
+        
+            <div >
+               
+              <h3>${country.name.common}</h3>
+              <button  onclick="loadCountryName('${country.name.official}')" class="m-2" id="detail-btn"><a id="link" href="#top">DETAILS</a></button>
+            
+            </div>
+            
+            
             
         `
         // h3.innerText = country.name.official;
@@ -28,9 +51,47 @@ const displayCountries = countries => {
         // div.appendChild(p);
         countryDiv.appendChild(div);
     });
+    // countryDiv.innerHTML = '';
+    document.getElementById('loader').style.display = 'none';
+}
+
+const displayCountries = countries => {
+    const countryDiv = document.getElementById('countries');
+
+    // for(const country of countries){
+
+    // }
+    countryDiv.innerHTML = '';
+    countries.forEach(country => {
+        const h3 = document.createElement('h3');
+        const div = document.createElement('div');
+        div.classList.add('country');
+        div.innerHTML = `
+        
+            <div >
+               
+              <h3>${country.name.common}</h3>
+              <button  onclick="loadCountryName('${country.name.official}')" class="m-2" id="detail-btn"><a id="link" href="#top">DETAILS</a></button>
+            
+            </div>
+            
+            
+            
+        `
+        // h3.innerText = country.name.official;
+        // div.appendChild(h3);
+        // const p =document.createElement('p');
+        // p.innerText = country.capital;
+        // div.appendChild(p);
+        countryDiv.appendChild(div);
+    });
+    // countryDiv.innerHTML = '';
+    document.getElementById('loader').style.display = 'none';
 }
 
 const loadCountryName = name => {
+    document.getElementById('loader').style.display = 'block';
+
     fetch(`https://restcountries.com/v3.1/name/${name}`)
         .then(res => res.json())
         .then(data => displayCountryDetail(data))
@@ -51,4 +112,9 @@ const displayCountryDetail = country => {
         <p>POPULATION: ${country[0].population}</p>
         
     `
+
+    document.getElementById('loader').style.display = 'none';
+
 }
+
+
